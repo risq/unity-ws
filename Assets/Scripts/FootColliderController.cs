9895f7b1fa3e08a7f2b1ff8706eadd3a343ad84c
risq <valentin.ledrapier@gmail.com>
2015-10-27 11:17:06 +0100

    Better movements & jumping

---
+++ Assets/Scripts/FootColliderController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootColliderController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.SendMessage("OnFootCollides", other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        player.SendMessage("OnFootStopsColliding", other);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,35 +5,39 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject characterSpriteObject;
 
-    enum STATE { STATE_IDLE, STATE_WALKING, STATE_JUMPING, STATE_CROUCHING };
-    int currentState;
     bool isGrounded = false;
     bool isJumping = false;
     bool isMoving = false;
     bool isFalling = false;
     string direction = "right";
+    float currentMaxJumpHeight = 0;
 
     Animator _animator;
     Rigidbody2D _rigidBody2D;
 
     const float MOVE_SPEED = 200;
-    const float JUMP_SPEED = 3000;
-    const float MAX_JUMP_HEIGHT = 0.1f;
+    const float JUMP_SPEED = 150;
+    const float MAX_JUMP_HEIGHT = 2f;
 
     // Use this for initialization
     void Start ()
     {
         _animator = characterSpriteObject.GetComponent<Animator>();
         _rigidBody2D = GetComponent<Rigidbody2D>();
-	}
+        IsGrounded = true;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded)
+        {
             IsJumping = true;
-        else
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
             IsJumping = false;
+        }
       
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -53,23 +57,25 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        if (IsFalling && _rigidBody2D.velocity.y < 0)
+        if (IsJumping)
         {
-            _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, _rigidBody2D.velocity.y / 2);
-        }
-        else if (IsJumping)
-        {
-            if (IsGrounded)
-                IsGrounded = false;
-
-            Debug.Log(transform.position.y);
-
-            if (transform.position.y < MAX_JUMP_HEIGHT) 
-                _rigidBody2D.AddForce(new Vector2(0, JUMP_SPEED * Time.fixedDeltaTime));
-            else
+            if (transform.position.y >= currentMaxJumpHeight)
             {
                 IsFalling = true;
+                IsJumping = false;
+                _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, 0);
             }
+            else
+            {
+                Debug.Log(currentMaxJumpHeight);
+                _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, JUMP_SPEED * (currentMaxJumpHeight / transform.position.y) * Time.fixedDeltaTime);
+            }
+        }
+
+        if (IsFalling)
+        {
+            _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, _rigidBody2D.velocity.y - 0.1f);
+            Debug.Log(_rigidBody2D.velocity.y);
         }
 
         if (IsMoving)
@@ -85,31 +91,25 @@ public class PlayerController : MonoBehaviour {
         }
         else
         {
-            _rigidBody2D.velocity = new Vector2(0, _rigidBody2D.velocity.y);
+            _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x * 0.8f, _rigidBody2D.velocity.y);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnFootCollides(Collider2D other)
     {
         IsGrounded = true;
         IsFalling = false;
-        //Debug.Log("Collision");
     }
 
-    public int CurrentState
+    void OnFootStopsColliding(Collider2D other)
     {
-        get
-        {
-            return currentState;
-        }
-        set
-        {
-            if (currentState != value)
-            {
-                currentState = value;
-                _animator.SetInteger("state", value);
-            }
-        }
+        IsGrounded = false;
+    }
+
+    void OnSideCollides()
+    {
+        IsGrounded = true;
+        IsFalling = false;
     }
 
     public string Direction
@@ -180,6 +180,16 @@ public class PlayerController : MonoBehaviour {
             {
                 isJumping = value;
                 _animator.SetBool("isJumping", value);
+
+                if (isJumping)
+                {
+                    currentMaxJumpHeight = transform.position.y + MAX_JUMP_HEIGHT;
+                }
+                else if (!IsGrounded)
+                {
+                    IsFalling = true;
+                    _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, _rigidBody2D.velocity.y / 2);
+                }
             }
         }
     }
